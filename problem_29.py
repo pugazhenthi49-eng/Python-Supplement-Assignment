91041759c97fa6abe314d3388da32563ed163523
Pugazhenthi <twisterg49@gmail.com>
2026-01-30 15:19:51 +0530

    Correct print statement for add_to_list
    Fix print statement for add_to_list function call.

--- problem_29.py
+++ problem_29.py
@@ -1,10 +1,8 @@
 # Problem 29: Function with default argument
 # Find and fix the error
-
 def add_to_list(item, lst=[]):
     lst.append(item)
     return lst
-
 print(add_to_list(1))
 print(add_to_list(2))
 print(add_to_list(3))
