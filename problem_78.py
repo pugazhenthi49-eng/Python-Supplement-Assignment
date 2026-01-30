ac6857e5962e807d7e6c84a3b8a6c93aca24e489
Pugazhenthi <twisterg49@gmail.com>
2026-01-30 17:46:45 +0530

    Update problem_78.py

--- problem_78.py
+++ problem_78.py
@@ -9,6 +9,5 @@ def find_duplicates(lst):
             duplicates.append(item)
         seen.add(item)
     return duplicates
-
 numbers = [1, 2, 3, 2, 4, 3, 5]
 print(f"Duplicates: {find_duplicates(numbers)}")
