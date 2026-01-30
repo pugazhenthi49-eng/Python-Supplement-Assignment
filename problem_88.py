6832b48648dc458a57c16c8ea7109e4b683a7d2f
Pugazhenthi <twisterg49@gmail.com>
2026-01-30 17:57:06 +0530

    Update problem_88.py

--- problem_88.py
+++ problem_88.py
@@ -1,12 +1,10 @@
 # Problem 88: Find all substrings of a string
 # Find and fix the error
-
 def all_substrings(text):
     substrings = []
     for i in range(len(text)):
         for j in range(i+1, len(text)):
             substrings.append(text[i:j])
     return substrings
-
 word = "abc"
 print(f"All substrings: {all_substrings(word)}")
