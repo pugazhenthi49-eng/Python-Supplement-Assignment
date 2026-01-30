154224453f85762a32c40c30a3e1bed2641347c2
Pugazhenthi <twisterg49@gmail.com>
2026-01-30 18:00:28 +0530

    Update problem_95.py

--- problem_95.py
+++ problem_95.py
@@ -1,6 +1,5 @@
 # Problem 95: Convert Roman to Integer
 # Find and fix the error
-
 def roman_to_int(s):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
