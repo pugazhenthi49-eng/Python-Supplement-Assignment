f919c3b37801702a573adc7369430fcfc00c3a07
Pugazhenthi <twisterg49@gmail.com>
2026-01-30 17:54:55 +0530

    Update problem_85.py

--- problem_85.py
+++ problem_85.py
@@ -8,6 +8,5 @@ def bubble_sort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
-
 numbers = [64, 34, 25, 12, 22, 11, 90]
 print(f"Sorted: {bubble_sort(numbers)}")
