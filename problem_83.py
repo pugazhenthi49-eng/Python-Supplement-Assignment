31341104ecf35b5a8c9131c59f04c4ed40cde0a6
Pugazhenthi <twisterg49@gmail.com>
2026-01-30 17:53:35 +0530

    Update problem_83.py

--- problem_83.py
+++ problem_83.py
@@ -1,9 +1,7 @@
 # Problem 83: Find kth smallest element
 # Find and fix the error
-
 def kth_smallest(arr, k):
     sorted_arr = sorted(arr)
     return sorted_arr[k]
-
 numbers = [7, 10, 4, 3, 20, 15]
 print(f"3rd smallest: {kth_smallest(numbers, 3)}")
